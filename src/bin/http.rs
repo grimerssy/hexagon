334a3baa731e0d4ab2935a79406d230f6035ad9b
grimerssy <grimerssy@gmail.com>
2023-08-24 19:00:21 +0200

    minor fixes

--- src/bin/http.rs
+++ src/bin/http.rs
@@ -1,9 +1,9 @@
-use hexagon::{init_config, init_telemetry, HttpServer};
+use hexagon::{init_config, init_telemetry, App, HttpServer};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let config = init_config()?;
-    let app = hexagon::App::new(init_config()?)?;
+    let app = App::new(init_config()?)?;
     init_telemetry()?;
     HttpServer::run(config, app).await
 }
