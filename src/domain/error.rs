3890f6719f58be5796c0595e905cca5da6ac62db
grimerssy <grimerssy@gmail.com>
2023-09-04 15:09:31 +0200

    fix errors display messages

--- src/domain/error.rs
+++ src/domain/error.rs
@@ -4,6 +4,6 @@ pub type Result<T> = std::result::Result<T, Error>;
 pub enum Error {
     #[error("email is taken")]
     EmailTaken,
-    #[error(transparent)]
+    #[error("an unexpected error occurred")]
     Unexpected(#[from] anyhow::Error),
 }
